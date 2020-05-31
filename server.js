381b9106b90515f6e60e12329c8446a6a1b52af4
1715802 <1715802@aluno.univesp.br>
2020-05-31 15:31:48 -0300

    test8

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-//Importa as dependências que acabamos de instalar
-const express = require('express');
-
-const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/calculodesignacao'));
+app.use(express.static('./dist/calculodesignacao'));
 
 app.get('/*', function(req,res) {
     
