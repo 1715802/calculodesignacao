2bf5d03cd501fe781653fa800687e4077072ebd7
1715802 <1715802@aluno.univesp.br>
2020-05-31 15:04:04 -0300

    test6

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/calculodesignacao'));
+app.use(express.static(__dirname + '/dist/calculodesignacao'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/calculodesignacao/index.html'));
+res.sendFile(path.join(__dirname+'/dist/calculodesignacao/index.html'));
 });
 
 // Inicia a aplicação pela porta configurada
